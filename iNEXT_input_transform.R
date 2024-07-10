6ef81586385866dfa9c27d191ebfc1f7c857b169
Maanas Nukala <manukala6@gmail.com>
2024-07-10 19:56:32 -0400

    Fix output formatting

--- iNEXT_input_transform.R
+++ iNEXT_input_transform.R
@@ -2,28 +2,33 @@ install.packages("readr")
 library(readr)
 install.packages("dplyr")
 library(dplyr)
+library(iNEXT)
 
-setwd("/fs/ess/PAS1032/CB/2024/anemone_maps/test")
+setwd(getwd())
+csv_folder <- paste0(getwd(), "/largelisttest")
+out_folder <- paste0(getwd(), "/iNEXT_output")
 
-csv_files <- list.files(path = "/fs/ess/PAS1032/CB/2024/anemone_maps/test", pattern = "*.csv", full.names = TRUE)
+csv_files <- list.files(
+  path = (csv_folder),
+  pattern = "*.csv", 
+  full.names = TRUE
+)
 
 print(csv_files)
 
 large_list2 <- list()
 
 for (csv_file in csv_files) { 
-  
-  data <- read_csv("/fs/ess/PAS1032/CB/2024/anemone_maps/test/10_1.csv", col_names = FALSE)
+  data <- read_csv(csv_file, col_names = FALSE)
   header <- data[1, 1] 
   values <- as.integer(pull(data[-1, ], 1))
-  large_list2[[as.character(header)]] <- values 
+  large_list2[[as.character(header)]] <- values
 }
 
-data.files	<- list.files(path="/fs/ess/PAS1032/CB/2024/anemone_maps/test", pattern=".csv", full.name=T)
+data.files	<- list.files(path=csv_folder, pattern=".csv", full.name=T)
 print(data.files)
 
-#out.names	<- paste("/fs/ess/PAS1032/CB/2024/anemone_maps/iNEXT_output", "iNEXT_", sub(".*/", "", data.files), sep="")
-out.names <- list(test1="test1.Rdata", test2="test2.Rdata")
+out.names	<- paste(out_folder, "iNEXT_", sub(".*/", "", data.files), sep="")
 
 for(j in 1:length(data.files)){
   #load(data.files[j])
